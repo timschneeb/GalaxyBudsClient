b0a89b015b06c7a63436e98950d8ada93c154e9d
Tim Schneeberger <thebone.main@gmail.com>
2020-07-18 00:10:14 +0200

    Update Constants.cs

--- Galaxy Buds Client/model/Constants.cs
+++ Galaxy Buds Client/model/Constants.cs
@@ -65,7 +65,7 @@ namespace Galaxy_Buds_Client.model
         {
             public enum Universal
             {
-                [Description("Voice Assistant (Android only)")]
+                [Description("Voice Assistant")]
                 VoiceAssistant,
                 [Description("Quick Ambient Sound")]
                 QuickAmbientSound,
