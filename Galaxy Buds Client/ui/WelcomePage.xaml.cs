e09f3fe2ac599a5bb3748b1b0d487e0c048b54cb
ThePBone <thebone.main@gmail.com>
2020-08-07 18:37:03 +0200

    Add language selection to welcome page + fix system/self-test header spinner objects

--- Galaxy Buds Client/ui/WelcomePage.xaml.cs
+++ Galaxy Buds Client/ui/WelcomePage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,8 +14,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Galaxy_Buds_Client.message;
+using Galaxy_Buds_Client.model;
+using Galaxy_Buds_Client.model.Constants;
 using Galaxy_Buds_Client.parser;
 using Galaxy_Buds_Client.util;
+using Galaxy_Buds_Client.util.DynamicLocalization;
 
 namespace Galaxy_Buds_Client.ui
 {
@@ -34,6 +38,7 @@ namespace Galaxy_Buds_Client.ui
         public override void OnPageShown()
         {
             _mainWindow.SetOptionsEnabled(false);
+            LocaleBorder.ContextMenu = GenerateMenu(LocaleBorder);
             DarkMode.Switch.SetChecked(Properties.Settings.Default.DarkMode);
         }
 
@@ -51,6 +56,56 @@ namespace Galaxy_Buds_Client.ui
             DarkModeHelper.SetState(e);
             Properties.Settings.Default.DarkMode = e;
             Properties.Settings.Default.Save();
+        }
+
+        private void Language_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Border el = (Border)sender;
+            el.ContextMenu.IsOpen = true;
+        }
+
+        private ContextMenu GenerateMenu(UIElement target)
+        {
+            ContextMenu ctxMenu = new ContextMenu();
+            ctxMenu.Style = (Style)FindResource("ContextMenuStyle");
+            ctxMenu.PlacementTarget = target;
+            ctxMenu.Placement = PlacementMode.Bottom;
+
+            bool first = true;
+            foreach (int value in Enum.GetValues(typeof(Locale)))
+            {
+                if (value == (int)Locale.custom && !Loc.IsTranslatorModeEnabled())
+                    continue;
+
+                if (!first)
+                    Menu_AddSeparator(ctxMenu);
+                else
+                    first = false;
+
+                Menu_AddItem(ctxMenu, (Locale)value);
+            }
+
+            return ctxMenu;
+        }
+
+        private void Menu_AddItem(ContextMenu c, Locale loc)
+        {
+            MenuItem m = new MenuItem();
+            m.Header = loc.GetDescription();
+            m.Click += delegate
+            {
+                Properties.Settings.Default.CurrentLocale = loc;
+                Properties.Settings.Default.Save();
+                Loc.Load();
+            };
+            m.Style = (Style)FindResource("MenuItemStyle");
+            c.Items.Add(m);
+        }
+        private void Menu_AddSeparator(ContextMenu c)
+        {
+            Separator s = new Separator();
+            s.Style = (Style)FindResource("SeparatorStyle");
+            c.Items.Add(s);
         }
     }
 }
