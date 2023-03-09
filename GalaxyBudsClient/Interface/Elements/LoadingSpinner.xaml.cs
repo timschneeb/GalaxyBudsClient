64c708ca291d8e60b6415a2330e34ec07341ec74
StegBrind <steg-brind@mailfence.com>
2023-03-09 14:13:39 +0200

    Improve LoadingSpinner performance

--- GalaxyBudsClient/Interface/Elements/LoadingSpinner.xaml.cs
+++ GalaxyBudsClient/Interface/Elements/LoadingSpinner.xaml.cs
@@ -1,13 +1,31 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace GalaxyBudsClient.Interface.Elements
 {
     public class LoadingSpinner : UserControl
     {
+        private readonly Image _spinnerImage;
+
         public LoadingSpinner()
         {
             AvaloniaXamlLoader.Load(this);
+
+            _spinnerImage = this.FindControl<Image>("Spinner");
+
+            PropertyChanged += (o, e) =>
+            {
+                if (e.Property.Name == nameof(IsVisible))
+                {
+                    if (e.NewValue != null)
+                    {
+                        if ((bool)e.NewValue)
+                            _spinnerImage.IsVisible = true;
+                        else
+                            _spinnerImage.IsVisible = false;
+                    }
+                }
+            };
         }
     }
 }
