619ac42dd62d8f13808e7ad04f3610a94830d72f
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-07 16:32:58 +0100

    refactor: cleanup DialogLauncher.cs

---
+++ GalaxyBudsClient/Utils/Interface/WindowLauncher.cs
@@ -0,0 +1,40 @@
+using System;
+using Avalonia.Controls;
+using GalaxyBudsClient.InterfaceOld.Developer;
+
+namespace GalaxyBudsClient.Utils.Interface
+{
+    public static class WindowLauncher
+    {
+        private static DevTools? _devTools;
+        private static TranslatorTools? _translatorTools;
+
+        public static void ShowDevTools(Window? parent = null) => ShowAsSingleInstance(ref _devTools, parent);
+        public static void ShowTranslatorTools(Window? parent = null) => ShowAsSingleInstance(ref _translatorTools, parent);
+        
+        private static void ShowAsSingleInstance<T>(ref T? target, Window? parent = null) where T : Window, new()
+        {
+            target ??= new T();
+            try
+            {
+                Show(target);
+            }
+            catch (InvalidOperationException)
+            {
+                // Old window object has been closed and cannot be reused 
+                target = new T();
+                Show(target);
+            }
+            return;
+
+            void Show(T target)
+            {
+                if (parent == null)
+                    target.Show();
+                else
+                    target.Show(parent);
+            }
+        }
+
+    }
+}
