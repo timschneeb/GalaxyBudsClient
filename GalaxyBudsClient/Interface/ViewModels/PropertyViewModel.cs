21398a9eb7f3bb3badf71f281e3247109b6ce9e6
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-16 19:28:27 +0100

    refactor: move old view models

---
+++ GalaxyBudsClient/Interface/ViewModels/PropertyViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GalaxyBudsClient.Interface.ViewModels
+{
+    public class PropertyViewModel(string key, string value)
+    {
+        public string Key { get; set; } = key;
+        public string Value { get; set; } = value;
+    }
+}
