3c08893c00936a8c34b52e82f0ba94452060860e
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-16 18:24:36 +0100

    refactor: cleanup enum attribute extensions

---
+++ GalaxyBudsClient/Model/Attributes/AttributeExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace GalaxyBudsClient.Model.Attributes
+{
+    public static class AttributeExtensions
+    {
+        private static TAttr? GetEnumAttribute<TAttr,TEnum>(this TEnum e) where TAttr : Attribute where TEnum : struct, IConvertible
+        { 
+            if(e is not Enum)
+                throw new ArgumentException("Enum type expected");
+            
+            var type = e.GetType();
+            foreach (var obj in Enum.GetValues(type))
+            {
+                if (obj == null || (int)obj != e.ToInt32(CultureInfo.InvariantCulture)) 
+                    continue;
+                
+                if (type.GetMember(type.GetEnumName((int) obj) ?? string.Empty)[0]
+                        .GetCustomAttributes(typeof(TAttr), false)
+                        .FirstOrDefault() is TAttr attr)
+                {
+                    return attr;
+                }
+            }
+
+            return null;
+        }
+
+        public static string GetDescription<T>(this T e) where T : struct, IConvertible
+            => e.GetEnumAttribute<DescriptionAttribute, T>()?.Description ?? string.Empty;
+
+        public static string GetLocalizableKey<T>(this T e) where T : struct, IConvertible
+            => e.GetEnumAttribute<LocalizedDescriptionAttribute, T>()?.Key ?? string.Empty;
+        
+        public static bool IsHidden<T>(this T e) where T : struct, IConvertible
+            => e.GetEnumAttribute<HiddenAttribute, T>() != null;
+        
+        public static ModelMetadataAttribute? GetModelMetadata<T>(this T e) where T : struct, IConvertible
+            => e.GetEnumAttribute<ModelMetadataAttribute, T>();
+    }
+}
--- GalaxyBudsClient/Model/Attributes/HiddenAttribute.cs
+++ GalaxyBudsClient/Model/Attributes/HiddenAttribute.cs
@@ -1,44 +1,6 @@
 ﻿using System;
-using System.Globalization;
-using System.Linq;
 
 namespace GalaxyBudsClient.Model.Attributes
 {
-    class HiddenAttribute : Attribute
-    {
-    }
-    
-    static class HiddenAttributeExtension
-    {
-        public static bool IsHidden<T>(this T e) where T : IConvertible
-        {
-            if (e is Enum)
-            {
-                Type type = e.GetType();
-                Array values = Enum.GetValues(type);
-
-                foreach (var val in values)
-                {
-                    if (val == null)
-                    {
-                        continue;
-                    }
-                    
-                    if ((int)val == e.ToInt32(CultureInfo.InvariantCulture))
-                    {
-                        var memInfo = type.GetMember(type.GetEnumName(val) ?? string.Empty);
-
-                        if (memInfo[0]
-                            .GetCustomAttributes(typeof(HiddenAttribute), false)
-                            .FirstOrDefault() is HiddenAttribute hiddenAttribute)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-    }
+    public class HiddenAttribute : Attribute {}
 }
