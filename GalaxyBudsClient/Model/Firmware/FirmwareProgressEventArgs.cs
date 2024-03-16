f14600b1357e85fbfb530df4060ff69e9b0e0b47
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-16 19:25:59 +0100

    refactor: use primary constructors

--- GalaxyBudsClient/Model/Firmware/FirmwareProgressEventArgs.cs
+++ GalaxyBudsClient/Model/Firmware/FirmwareProgressEventArgs.cs
@@ -2,17 +2,11 @@ using System;
 
 namespace GalaxyBudsClient.Model.Firmware
 {
-    public class FirmwareProgressEventArgs : EventArgs
+    public class FirmwareProgressEventArgs(int percent, long currentEstimatedByteCount, long totalByteCount)
+        : EventArgs
     {
-        public FirmwareProgressEventArgs(int percent, long currentEstimatedByteCount, long totalByteCount)
-        {
-            Percent = percent;
-            CurrentEstimatedByteCount = currentEstimatedByteCount;
-            TotalByteCount = totalByteCount;
-        }
-
-        public int Percent { get; }
-        public long CurrentEstimatedByteCount { get; }
-        public long TotalByteCount { get; }
+        public int Percent { get; } = percent;
+        public long CurrentEstimatedByteCount { get; } = currentEstimatedByteCount;
+        public long TotalByteCount { get; } = totalByteCount;
     }
 }
