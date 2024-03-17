d6883229be6675aed083b5a00172ebde3335b865
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-17 19:50:19 +0100

    fix: remove debug code

--- GalaxyBudsClient/Program.cs
+++ GalaxyBudsClient/Program.cs
@@ -35,7 +35,7 @@ namespace GalaxyBudsClient
                 .WriteTo.File(PlatformUtils.CombineDataPath("application.log"))
                 .WriteTo.Console();
 
-            config = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("VERBOSE")) ||  true ? // TODO   ?
+            config = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("VERBOSE")) ?
                 config.MinimumLevel.Verbose() : config.MinimumLevel.Debug();
             
             // Divert program startup flow if the app was started with arguments (except /StartMinimized)
