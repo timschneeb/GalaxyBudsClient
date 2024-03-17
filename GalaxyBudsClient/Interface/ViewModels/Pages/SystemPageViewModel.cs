47cac24984fce063bafd4e7c68c9399996cb4418
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-17 17:12:13 +0100

    refactor: implement new self-test UI

--- GalaxyBudsClient/Interface/ViewModels/Pages/SystemPageViewModel.cs
+++ GalaxyBudsClient/Interface/ViewModels/Pages/SystemPageViewModel.cs
@@ -7,12 +7,6 @@ namespace GalaxyBudsClient.Interface.ViewModels.Pages;
 public class SystemPageViewModel : MainPageViewModelBase
 {
     public override Control CreateView() => new SystemPage();
-    
-    public SystemPageViewModel()
-    {
-        
-    }
-
     public override string TitleKey => "mainpage_system";
     public override Symbol IconKey => Symbol.Apps;
     public override bool ShowsInFooter => false;
