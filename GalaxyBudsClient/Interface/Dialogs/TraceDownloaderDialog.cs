e644cf0d865374f9ef1e98df2ac7f2e3717ff6bd
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-18 13:30:19 +0100

    refactor: move dialogs into separate classes

---
+++ GalaxyBudsClient/Interface/Dialogs/TraceDownloaderDialog.cs
@@ -0,0 +1,142 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Threading;
+using FluentAvalonia.UI.Controls;
+using GalaxyBudsClient.Message;
+using GalaxyBudsClient.Platform;
+using GalaxyBudsClient.Utils.Extensions;
+using GalaxyBudsClient.Utils.Interface.DynamicLocalization;
+using Serilog;
+using Symbol = FluentIcons.Common.Symbol;
+using SymbolIconSource = FluentIcons.Avalonia.Fluent.SymbolIconSource;
+
+namespace GalaxyBudsClient.Interface.Dialogs;
+
+public class TraceDownloaderDialog : TaskDialog
+{
+    private readonly TextBlock _content;
+    private readonly TaskDialogButton _closeButton;
+
+    public TraceDownloaderDialog()
+    {
+        _closeButton = new TaskDialogButton(Loc.Resolve("cancel"), TaskDialogStandardResult.Close);
+
+        Header = Loc.Resolve("coredump_header");
+        Buttons = [_closeButton];
+        IconSource = new SymbolIconSource { Symbol = Symbol.Bug };
+        SubHeader = Loc.Resolve("coredump_dl_progress_prepare");
+        XamlRoot = MainWindow2.Instance;
+        ShowProgressBar = true;
+        Content = _content = new TextBlock
+        {
+            Text = string.Empty,
+            TextWrapping = TextWrapping.Wrap,
+            MaxWidth = 600
+        };
+            
+        SetProgressBarState(0, TaskDialogProgressState.Indeterminate);
+
+        DeviceLogManager.Instance.ProgressUpdated += OnProgressUpdated;
+        DeviceLogManager.Instance.Finished += OnFinished;
+        BluetoothImpl.Instance.Disconnected += OnDisconnected;
+    }
+
+    protected override Type StyleKeyOverride => typeof(TaskDialog);
+
+    protected override async void OnClosing(TaskDialogClosingEventArgs args)
+    {
+        BluetoothImpl.Instance.Disconnected -= OnDisconnected;
+        DeviceLogManager.Instance.Finished -= OnFinished;
+        DeviceLogManager.Instance.ProgressUpdated -= OnProgressUpdated;
+        
+        await DeviceLogManager.Instance.CancelDownload();
+        base.OnClosing(args);
+    }
+    
+    public async Task BeginDownload()
+    {
+        await DeviceLogManager.Instance.BeginDownloadAsync();
+        await ShowAsync(true);
+    }
+
+    private void OnFinished(object? s, LogDownloadFinishedEventArgs ev)
+    {
+        Dispatcher.UIThread.Post(async () =>
+        {
+            SubHeader = Loc.Resolve("coredump_dl_progress_finished");
+            SetProgressBarState(100, TaskDialogProgressState.Normal);
+
+            var path = await MainWindow2.Instance.OpenFolderPickerAsync(
+                Loc.Resolve("coredump_dl_save_dialog_title"));
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            ev.CoreDumpPaths.ForEach(x => CopyDump(x, path));
+            ev.TraceDumpPaths.ForEach(x => CopyDump(x, path));
+
+            await new MessageBox
+            {
+                Title = Loc.Resolve("coredump_dl_save_success_title"),
+                Description = string.Format(Loc.Resolve("coredump_dl_save_success"),
+                    ev.CoreDumpPaths.Count + ev.TraceDumpPaths.Count, path)
+            }.ShowAsync();
+        }, DispatcherPriority.Normal);
+    }
+
+    private void OnDisconnected(object? o, string s)
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            SubHeader = Loc.Resolve("connlost_disconnected");
+            SetProgressBarState(100, TaskDialogProgressState.Error);
+            _content.Text = string.Empty;
+            _closeButton.Text = Loc.Resolve("window_close");
+        }, DispatcherPriority.Normal);
+    }
+        
+    private void OnProgressUpdated(object? s, LogDownloadProgressEventArgs ev)
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            if (ev.DownloadType == LogDownloadProgressEventArgs.Type._Switching)
+            {
+                SubHeader = Loc.Resolve("coredump_dl_progress_prepare");
+                SetProgressBarState(0, TaskDialogProgressState.Indeterminate);
+                _content.Text = string.Empty;
+            }
+            else
+            {
+                SubHeader = string.Format(Loc.Resolve($"coredump_dl_{(ev.DownloadType == LogDownloadProgressEventArgs.Type.Coredump ? "coredump" : "trace")}_progress"), 
+                    Math.Ceiling((double)ev.CurrentByteCount / ev.TotalByteCount * 100));
+                _content.Text = string.Format(
+                        Loc.Resolve("coredump_dl_progress_size"),
+                        ev.CurrentByteCount.ToString(),
+                        ev.TotalByteCount.ToString())
+                    .Replace("(", "")
+                    .Replace(")", "");
+                SetProgressBarState(Math.Ceiling(((double)ev.CurrentByteCount / ev.TotalByteCount) * 100), TaskDialogProgressState.Normal);
+            }
+        }, DispatcherPriority.Normal);
+    }
+        
+    private static async void CopyDump(string source, string targetDir)
+    {
+        try
+        {
+            File.Copy(source, Path.Combine(targetDir, Path.GetFileName(source)));
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "CoreDump: Failed to copy dumps");
+            await new MessageBox
+            {
+                Title = Loc.Resolve("coredump_dl_save_fail_title"),
+                Description = string.Format(Loc.Resolve("coredump_dl_save_fail"), ex.Message)
+            }.ShowAsync();
+        }
+    }
+    
+}
--- GalaxyBudsClient/Interface/Pages/SystemPage.axaml.cs
+++ GalaxyBudsClient/Interface/Pages/SystemPage.axaml.cs
@@ -1,23 +1,17 @@
-using System;
-using System.IO;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Media;
-using Avalonia.Threading;
 using FluentAvalonia.UI.Controls;
 using GalaxyBudsClient.Interface.Dialogs;
 using GalaxyBudsClient.Interface.Services;
 using GalaxyBudsClient.Interface.ViewModels.Pages;
 using GalaxyBudsClient.Message;
-using GalaxyBudsClient.Message.Decoder;
 using GalaxyBudsClient.Platform;
 using GalaxyBudsClient.Utils.Extensions;
 using GalaxyBudsClient.Utils.Interface.DynamicLocalization;
-using Serilog;
-using Symbol = FluentIcons.Common.Symbol;
 
 namespace GalaxyBudsClient.Interface.Pages;
 
@@ -100,7 +94,6 @@ public partial class SystemPage : BasePage<SystemPageViewModel>
     private async void OnPairingModeClicked(object? sender, RoutedEventArgs e)
     {
         await BluetoothImpl.Instance.SendRequestAsync(SppMessage.MessageIds.UNK_PAIRING_MODE);
-        await BluetoothImpl.Instance.DisconnectAsync();
         await new MessageBox
         {
             Title = Loc.Resolve("connlost_disconnected"),
@@ -110,113 +103,7 @@ public partial class SystemPage : BasePage<SystemPageViewModel>
     
     private async void OnSelfTestClicked(object? sender, RoutedEventArgs e)
     {
-        var closeButton = new TaskDialogButton(Loc.Resolve("cancel"), TaskDialogStandardResult.Close);
-        var cancelToken = new CancellationTokenSource();
-
-        var td = new TaskDialog
-        {
-            Header = Loc.Resolve("selftest_header"),
-            Buttons = [closeButton],
-            IconSource = new FluentIcons.Avalonia.Fluent.SymbolIconSource() { Symbol = Symbol.Beaker },
-            Content = new TextBlock()
-            {
-                TextWrapping = TextWrapping.Wrap,
-                Text = Loc.Resolve("system_waiting_for_device"),
-                MaxWidth = 600
-            },
-            XamlRoot = MainWindow2.Instance,
-            ShowProgressBar = true,
-        };
-            
-        try
-        {
-            td.SetProgressBarState(100, TaskDialogProgressState.Indeterminate);
-            _ = td.ShowAsync(true).ContinueWith(a =>
-            {
-                if (a.Result as TaskDialogStandardResult? == TaskDialogStandardResult.Close)
-                    cancelToken.Cancel();
-            }, cancelToken.Token);
-
-            SppMessageHandler.Instance.SelfTestResponse += OnSelfTestResponse;
-            await BluetoothImpl.Instance.SendRequestAsync(SppMessage.MessageIds.SELF_TEST);
-
-            // Wait for 10 seconds for the self test response
-            await Task.Delay(10000, cancelToken.Token);
-        }
-        catch (TaskCanceledException) {}
-      
-        SppMessageHandler.Instance.SelfTestResponse -= OnSelfTestResponse;
-
-        // If the self test response was not received, show an error message
-        if (td.IsVisible)
-            OnSelfTestResponse(null, null);
-        return;
-
-        TaskDialogCommand ResultWithSideAsTaskItem(string testKey, bool lPass, bool rPass)
-        {
-            return new TaskDialogCommand
-            {
-                Text = Loc.Resolve(testKey),
-                IconSource = new FluentIcons.Avalonia.Fluent.SymbolIconSource
-                {
-                    Symbol = !lPass || !rPass ? Symbol.Warning : Symbol.Checkmark
-                },
-                Description = $"{Loc.Resolve("left")}: {Loc.Resolve(lPass ? "selftest_pass" : "selftest_fail")}, " +
-                              $"{Loc.Resolve("right")}: {Loc.Resolve(rPass ? "selftest_pass" : "selftest_fail")}",
-                ClosesOnInvoked = false
-            };
-        }
-        
-        TaskDialogCommand ResultAsTaskItem(string testKey, bool pass)
-        {
-            return new TaskDialogCommand
-            {
-                Text = Loc.Resolve(testKey),
-                IconSource = new FluentIcons.Avalonia.Fluent.SymbolIconSource
-                {
-                    Symbol = !pass ? Symbol.Warning : Symbol.Checkmark
-                },
-                Description = Loc.Resolve(pass ? "selftest_pass" : "selftest_fail"),
-                ClosesOnInvoked = false
-            };
-        }
-        
-        void OnSelfTestResponse(object? s, SelfTestParser? parser)
-        {
-            cancelToken.Cancel();
-            td.Buttons[0].Text = Loc.Resolve("window_close");
-
-            if (parser == null)
-            {
-                td.SetProgressBarState(100, TaskDialogProgressState.Error);
-                ((TextBlock)td.Content).Text = Loc.Resolve("system_no_response");
-            }
-            else
-            {
-                _ = new TaskDialog
-                {
-                    Header = Loc.Resolve(parser is not { AllChecks: true } ? "selftest_fail_long" : "selftest_pass_long"),
-                    Buttons = td.Buttons,
-                    IconSource = td.IconSource,
-                    XamlRoot = MainWindow2.Instance,
-                    Commands = new[]
-                    {
-                        ResultAsTaskItem("system_hwver", parser.HardwareVersion),
-                        ResultAsTaskItem("system_swver", parser.SoftwareVersion),
-                        ResultAsTaskItem("system_touchver", parser.TouchFirmwareVersion),
-                        ResultWithSideAsTaskItem("system_btaddr", parser.LeftBluetoothAddress, parser.RightBluetoothAddress),
-                        ResultWithSideAsTaskItem("system_proximity", parser.LeftProximity, parser.RightProximity),
-                        ResultWithSideAsTaskItem("system_thermo", parser.LeftThermistor, parser.RightThermistor),
-                        ResultWithSideAsTaskItem("system_adc_soc", parser.LeftAdcSOC, parser.RightAdcSOC),
-                        ResultWithSideAsTaskItem("system_adc_voltage", parser.LeftAdcVCell, parser.RightAdcVCell),
-                        ResultWithSideAsTaskItem("system_adc_current", parser.LeftAdcCurrent, parser.RightAdcCurrent),
-                        ResultWithSideAsTaskItem("system_hall", parser.LeftHall, parser.RightHall),
-                        ResultWithSideAsTaskItem("system_accel", parser.AllLeftAccelerator, parser.AllRightAccelerator)
-                    }
-                }.ShowAsync(true);
-                td.Hide();
-            }
-        }
+        await new SelfTestDialog().ExecuteTestAsync();
     }
 
     private async void OnSpatialSensorTestClicked(object? sender, RoutedEventArgs e)
@@ -254,7 +141,7 @@ public partial class SystemPage : BasePage<SystemPageViewModel>
                              $"Roll={rpy[0]}\nPitch={rpy[1]}\nYaw={rpy[2]}\n";
         }
     }
-    
+
     private async void OnTraceDumpDownloadClicked(object? sender, RoutedEventArgs e)
     {
         var result = await new QuestionBox()
@@ -267,115 +154,6 @@ public partial class SystemPage : BasePage<SystemPageViewModel>
         if (!result)
             return;
         
-        // Begin download
-        var closeButton = new TaskDialogButton(Loc.Resolve("cancel"), TaskDialogStandardResult.Close);
-        var textBlock = new TextBlock
-        {
-            Text = string.Empty,
-            TextWrapping = TextWrapping.Wrap,
-            MaxWidth = 600
-        };
-        var td = new TaskDialog
-        {
-            Header = Loc.Resolve("coredump_header"),
-            Buttons = [closeButton],
-            IconSource = new FluentIcons.Avalonia.Fluent.SymbolIconSource { Symbol = Symbol.Bug },
-            SubHeader = Loc.Resolve("coredump_dl_progress_prepare"),
-            XamlRoot = MainWindow2.Instance,
-            ShowProgressBar = true,
-            Content = textBlock
-        };
-        td.SetProgressBarState(0, TaskDialogProgressState.Indeterminate);
-
-        DeviceLogManager.Instance.ProgressUpdated += OnProgressUpdated;
-        DeviceLogManager.Instance.Finished += OnFinished;
-        BluetoothImpl.Instance.Disconnected += OnDisconnected;
-
-        await DeviceLogManager.Instance.BeginDownloadAsync();
-        await td.ShowAsync(true);
-        
-        BluetoothImpl.Instance.Disconnected -= OnDisconnected;
-        DeviceLogManager.Instance.Finished -= OnFinished;
-        DeviceLogManager.Instance.ProgressUpdated -= OnProgressUpdated;
-        
-        await DeviceLogManager.Instance.CancelDownload();
-        return;
-        
-        void OnFinished(object? s, LogDownloadFinishedEventArgs ev)
-        {
-            Dispatcher.UIThread.Post(async () =>
-            {
-                td.SubHeader = Loc.Resolve("coredump_dl_progress_finished");
-                td.SetProgressBarState(100, TaskDialogProgressState.Normal);
-
-                var path = await MainWindow2.Instance.OpenFolderPickerAsync(
-                    Loc.Resolve("coredump_dl_save_dialog_title"));
-                if (string.IsNullOrEmpty(path))
-                    return;
-
-                ev.CoreDumpPaths.ForEach(x => CopyDump(x, path));
-                ev.TraceDumpPaths.ForEach(x => CopyDump(x, path));
-
-                await new MessageBox()
-                {
-                    Title = Loc.Resolve("coredump_dl_save_success_title"),
-                    Description = string.Format(Loc.Resolve("coredump_dl_save_success"),
-                        ev.CoreDumpPaths.Count + ev.TraceDumpPaths.Count, path)
-                }.ShowAsync();
-            }, DispatcherPriority.Normal);
-        }
-
-        void OnDisconnected(object? o, string s)
-        {
-            Dispatcher.UIThread.Post(() =>
-            {
-                td.SubHeader = Loc.Resolve($"connlost_disconnected");
-                td.SetProgressBarState(100, TaskDialogProgressState.Error);
-                textBlock.Text = string.Empty;
-                closeButton.Text = Loc.Resolve("window_close");
-            }, DispatcherPriority.Normal);
-        }
-        
-        void OnProgressUpdated(object? s, LogDownloadProgressEventArgs ev)
-        {
-            Dispatcher.UIThread.Post(() =>
-            {
-                if (ev.DownloadType == LogDownloadProgressEventArgs.Type._Switching)
-                {
-                    td.SubHeader = Loc.Resolve($"coredump_dl_progress_prepare");
-                    td.SetProgressBarState(0, TaskDialogProgressState.Indeterminate);
-                    textBlock.Text = string.Empty;
-                }
-                else
-                {
-                    td.SubHeader = string.Format(Loc.Resolve($"coredump_dl_{(ev.DownloadType == LogDownloadProgressEventArgs.Type.Coredump ? "coredump" : "trace")}_progress"), 
-                            Math.Ceiling((double)ev.CurrentByteCount / ev.TotalByteCount * 100));
-                    textBlock.Text = string.Format(
-                            Loc.Resolve("coredump_dl_progress_size"),
-                            ev.CurrentByteCount.ToString(),
-                            ev.TotalByteCount.ToString())
-                        .Replace("(", "")
-                        .Replace(")", "");
-                    td.SetProgressBarState(Math.Ceiling(((double)ev.CurrentByteCount / ev.TotalByteCount) * 100), TaskDialogProgressState.Normal);
-                }
-            }, DispatcherPriority.Normal);
-        }
-        
-        async void CopyDump(string source, string targetDir)
-        {
-            try
-            {
-                File.Copy(source, Path.Combine(targetDir, Path.GetFileName(source)));
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "CoreDump: Failed to copy dumps");
-                await new MessageBox()
-                {
-                    Title = Loc.Resolve("coredump_dl_save_fail_title"),
-                    Description = string.Format(Loc.Resolve("coredump_dl_save_fail"), ex.Message)
-                }.ShowAsync();
-            }
-        }
+        await new TraceDownloaderDialog().BeginDownload();
     }
 }
