317db66e3dd9d77fc8f9450982f1127484d234e5
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-19 23:16:50 +0100

    refactor: implement new hotkey recorder UI

---
+++ GalaxyBudsClient/Interface/ViewModels/Dialogs/HotkeyRecorderDialogViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Input;
+using GalaxyBudsClient.Utils.Extensions;
+using ReactiveUI.Fody.Helpers;
+
+namespace GalaxyBudsClient.Interface.ViewModels.Dialogs
+{
+    public class HotkeyRecorderDialogViewModel : ViewModelBase
+    {
+        public HotkeyRecorderDialogViewModel()
+        {
+            Hotkeys = new ObservableCollection<Key>();
+            Hotkeys.CollectionChanged += (_, _) => HotkeyPreview = Hotkeys.AsAvaloniaHotkeyString();
+        }
+        
+        [Reactive] public string HotkeyPreview { set; get; } = string.Empty;
+
+        public ObservableCollection<Key> Hotkeys { get; private set; }
+    }
+}
