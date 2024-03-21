43496fc7bd3f2cd1ca29db1d8e850193108234bb
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-21 18:23:26 +0100

    refactor: move TouchOption

--- GalaxyBudsClient/Model/Specifications/BudsDeviceSpec.cs
+++ GalaxyBudsClient/Model/Specifications/BudsDeviceSpec.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using GalaxyBudsClient.Model.Constants;
-using GalaxyBudsClient.Model.Touchpad;
+using GalaxyBudsClient.Model.Specifications.Touch;
 
 namespace GalaxyBudsClient.Model.Specifications
 {
--- GalaxyBudsClient/Model/Specifications/Touch/StandardTouchOption.cs
+++ GalaxyBudsClient/Model/Specifications/Touch/StandardTouchOption.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
 using GalaxyBudsClient.Model.Constants;
 
-namespace GalaxyBudsClient.Model.Touchpad
+namespace GalaxyBudsClient.Model.Specifications.Touch
 {
-    public class BudsLiveTouchOption : ITouchOption
+    public class StandardTouchOption : ITouchOption
     {
         public Dictionary<TouchOptions, byte> LookupTable => new()
         {
             {TouchOptions.VoiceAssistant, 1},
-            {TouchOptions.Anc, 2},
+            {TouchOptions.NoiseControl, 2},
             {TouchOptions.Volume, 3},
             {TouchOptions.SpotifySpotOn, 4},
             {TouchOptions.OtherL, 5},
