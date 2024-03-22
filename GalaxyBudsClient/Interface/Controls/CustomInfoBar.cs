37c7fa4ebd180c532370bcded4b8d95e867d1324
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-22 20:00:22 +0100

    refactor: better InfoBar style

---
+++ GalaxyBudsClient/Interface/Controls/CustomInfoBar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using FluentAvalonia.UI.Controls;
+
+namespace GalaxyBudsClient.Interface.Controls;
+
+/// <summary>
+/// Custom InfoBar with smaller text and less opacity
+/// </summary>
+public class CustomInfoBar : InfoBar
+{
+    protected override Type StyleKeyOverride => typeof(InfoBar);
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        if (e.NameScope.Find("Message") is TextBlock tb)
+        {
+            tb.Opacity = 0.8;
+            tb.FontSize = 13;
+        }
+
+        base.OnApplyTemplate(e);
+    }
+}
