7ad055d958ffff1bd42387507f2341054632c68d
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-22 16:48:35 +0100

    refactor: remove unused code

--- GalaxyBudsClient/Interface/Pages/BixbyRemapPage.axaml.cs
+++ GalaxyBudsClient/Interface/Pages/BixbyRemapPage.axaml.cs
@@ -1,6 +1,3 @@
-using Avalonia.Interactivity;
-using FluentAvalonia.UI.Controls;
-using GalaxyBudsClient.Interface.Services;
 using GalaxyBudsClient.Interface.ViewModels.Pages;
 
 namespace GalaxyBudsClient.Interface.Pages;
@@ -10,14 +7,5 @@ public partial class BixbyRemapPage : BasePage<BixbyRemapPageViewModel>
     public BixbyRemapPage()
     {
         InitializeComponent();
-        AddHandler(SettingsExpanderItem.ClickEvent, OnSettingsItemClicked);
-    }
-
-    private void OnSettingsItemClicked(object? sender, RoutedEventArgs e)
-    {
-        if (e.Source is not SettingsExpanderItem item)
-            return;
-        
-        NavigationService.Instance.Navigate(item.Name);
     }
 }
