399d60f772b7ab9fb6229007ef504d3691708aff
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-22 16:10:05 +0100

    fix: make EnumBindingSources locale aware using behavior

---
+++ GalaxyBudsClient/Interface/MarkupExtensions/LocalizationAwareBehavior.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Xaml.Interactivity;
+using GalaxyBudsClient.Interface.Controls;
+using GalaxyBudsClient.Utils.Interface.DynamicLocalization;
+
+namespace GalaxyBudsClient.Interface.MarkupExtensions;
+
+/*
+ * Workaround: Until UpdateSourceTrigger.Explicit is usable in Avalonia,
+ * this behavior is used to update the source of a ComboBox in case of localization changes. 
+ */
+public class LocalizationAwareBehavior : Behavior<Control>
+{
+    
+    public static readonly StyledProperty<Type> EnumTypeProperty =
+        AvaloniaProperty.Register<LocalizationAwareBehavior, Type>(nameof(EnumType));
+    
+    public Type EnumType
+    {
+        get => GetValue(EnumTypeProperty);
+        set => SetValue(EnumTypeProperty, value);
+    }
+    
+    /// <inheritdoc />
+    protected override void OnAttachedToVisualTree()
+    {
+        Loc.LanguageUpdated += OnLanguageUpdated;
+    }
+
+    /// <inheritdoc />
+    protected override void OnDetachedFromVisualTree()
+    {
+        Loc.LanguageUpdated -= OnLanguageUpdated;
+    }
+    
+    private void OnLanguageUpdated() => UpdateState();
+    
+    private void UpdateState()
+    {
+        // Handle combo box updates
+        if (AssociatedObject is SettingsComboBoxItem cb)
+        {
+            var selected = cb.SelectedValue;
+            cb.ItemsSource = Enum.GetValues(EnumType)
+                .OfType<object>()
+                .Where(obj => EnumType.GetMember(EnumType.GetEnumName((int)obj) ?? string.Empty)[0]
+                    .GetCustomAttributes(typeof(IgnoreDataMemberAttribute), false)
+                    .Length <= 0).ToArray();
+            cb.SelectedValue = selected;
+        }
+    }
+}
--- GalaxyBudsClient/Interface/Pages/SettingsPage.axaml.cs
+++ GalaxyBudsClient/Interface/Pages/SettingsPage.axaml.cs
@@ -37,19 +37,4 @@ public partial class SettingsPage : BasePage<SettingsPageViewModel>
     {
         BluetoothService.Instance.UnregisterDevice();
     }
-    
-    protected override void OnLanguageUpdated()
-    {
-        /* TODO:  Workaround because Avalonia does not expose enough of its Binding/MarkupExtension API
-                  to allow for dynamic language updates with ValueConverters.
-                  They have plans to implement a WPF-like API to obtain a BindingExpression from an existing Binding,
-                  which can be used to call expr.UpdateTarget(). 
-                  This could be implemented in an XAML custom behavior or markup extension */ 
-        AppTheme.ItemsSource = Enum.GetValues(typeof(DarkModes));
-        AppTheme.SelectedValue = Settings.Instance.DarkMode;
-        DynamicTrayIcon.ItemsSource = Enum.GetValues(typeof(DynamicTrayIconModes));
-        DynamicTrayIcon.SelectedValue = Settings.Instance.DynamicTrayIconMode;
-        PopupPosition.ItemsSource = Enum.GetValues(typeof(PopupPlacement));
-        PopupPosition.SelectedValue = Settings.Instance.Popup.Placement;
-    }
 }
