28d0e9884b37be617d1e41b622f650ecb57626bf
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-23 18:09:03 +0100

    feat: add UI for advanced touch call controls

--- GalaxyBudsClient/Message/Encoder/LockTouchpadEncoder.cs
+++ GalaxyBudsClient/Message/Encoder/LockTouchpadEncoder.cs
@@ -1,10 +1,14 @@
 using System.IO;
+using GalaxyBudsClient.Bluetooth;
+using GalaxyBudsClient.Model.Specifications;
+using GalaxyBudsClient.Platform;
 
 namespace GalaxyBudsClient.Message.Encoder;
 
 public static class LockTouchpadEncoder
 {
-    public static SppMessage Build(bool lockAll, bool tapOn, bool doubleTapOn, bool tripleTapOn, bool holdTapOn)
+    public static SppMessage Build(bool lockAll, bool tapOn, bool doubleTapOn, bool tripleTapOn, bool holdTapOn, 
+        bool doubleTapCallOn, bool holdTapCallOn)
     {
         var stream = new MemoryStream();
         var writer = new BinaryWriter(stream);
@@ -14,7 +18,13 @@ public static class LockTouchpadEncoder
         writer.Write(doubleTapOn);
         writer.Write(tripleTapOn);
         writer.Write(holdTapOn);
-            
+
+        if (BluetoothService.Instance.DeviceSpec.Supports(Features.AdvancedTouchLockForCalls))
+        {
+            writer.Write(doubleTapCallOn);
+            writer.Write(holdTapCallOn);
+        }
+
         var data = stream.ToArray();
         stream.Close();
             
