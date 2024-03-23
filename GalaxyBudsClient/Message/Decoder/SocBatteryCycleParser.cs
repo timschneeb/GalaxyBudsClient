a4e65f7f06ac0b45dfc531e1ab7e1f7eb90f6fa5
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-23 03:01:10 +0100

    feat: add SOC_BATTERY_CYCLE message support

---
+++ GalaxyBudsClient/Message/Decoder/SocBatteryCycleParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GalaxyBudsClient.Message.Decoder;
+
+internal class SocBatteryCycleParser : BaseMessageParser
+{
+    public override SppMessage.MessageIds HandledType => SppMessage.MessageIds.SOC_BATTERY_CYCLE;
+        
+    public long LeftCycles { set; get; }
+    public long RightCycles { set; get; }
+
+    public override void ParseMessage(SppMessage msg)
+    {
+        if (msg.Id != HandledType)
+            return;
+        
+        var payload = msg.Payload.AsSpan();
+        LeftCycles = BitConverter.ToInt64(payload[0..7]);
+        RightCycles = BitConverter.ToInt64(payload[8..15]);
+    }
+}
