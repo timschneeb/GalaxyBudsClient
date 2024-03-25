8876b90336c87897f94eb3e78aa13f6aac42ead5
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-26 00:29:26 +0100

    refactor: disable parts of the UI when the device is connected

---
+++ GalaxyBudsClient/Interface/MarkupExtensions/RequiresConnectedDeviceBehavior.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using Avalonia.Xaml.Interactivity;
+using GalaxyBudsClient.Platform;
+
+namespace GalaxyBudsClient.Interface.MarkupExtensions;
+
+/// <summary>
+/// Behavior that hides the control if no device is connected.
+/// </summary>
+/// <remarks>
+/// This behavior updates the <see cref="Control.IsEnabled"/> property of the associated control and may clash with existing bindings.
+/// </remarks>
+public class RequiresConnectedDeviceBehavior : Behavior<Control>
+{
+    /// <inheritdoc />
+    protected override void OnAttachedToVisualTree()
+    {
+        UpdateState();
+        BluetoothService.Instance.PropertyChanged += OnBluetoothPropertyChanged;
+    }
+
+    /// <inheritdoc />
+    protected override void OnDetachedFromVisualTree()
+    {
+        BluetoothService.Instance.PropertyChanged -= OnBluetoothPropertyChanged;
+    }
+
+    private void OnBluetoothPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(BluetoothService.Instance.IsConnected))
+        {
+            Dispatcher.UIThread.Post(UpdateState, DispatcherPriority.Normal);
+        }
+    }
+    
+    protected virtual void UpdateState()
+    {
+        if (AssociatedObject is null)
+            return;
+
+        AssociatedObject.IsEnabled = BluetoothService.Instance.IsConnected;
+    }
+}
