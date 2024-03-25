9783967c037ec893f1c51882c440fc6bf48f677f
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-03-25 23:21:34 +0100

    fix: ugly workaround for corners of hidden items in SettingsGroups

--- GalaxyBudsClient/Interface/Controls/SettingsGroup.cs
+++ GalaxyBudsClient/Interface/Controls/SettingsGroup.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 
 namespace GalaxyBudsClient.Interface.Controls;
@@ -18,6 +22,45 @@ public class SettingsGroup : ItemsControl
     private void ItemsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
     {
         IsVisible = ItemCount > 0;
-        Classes.Set("SingleItem", ItemCount == 1);
+
+        foreach (var control in e.NewItems?.OfType<Control>() ?? Enumerable.Empty<Control>())
+        {
+            control.GetPropertyChangedObservable(IsVisibleProperty)
+                .Subscribe(_ => UpdateCorners());
+        }
+        
+        UpdateCorners();
+    }
+    
+    private void UpdateCorners()
+    {
+        // Ugly workaround to fix rounded corners if first/last children are hidden.
+        // ClipToBounds doesn't work with rounded corners at the moment.
+        
+        // Handle single items
+        var visibleItems = Items.OfType<Control>().Where(x => x.IsVisible).ToList();
+        if(visibleItems.Count == 1 && visibleItems.First() is {} single)
+        {
+            single.Classes.Set("SingleItem", true);
+            single.Classes.Set("First", false);
+            single.Classes.Set("Last", false);
+            return;
+        }
+        
+        var firstItemFound = false;
+        foreach (var item in Items.OfType<Control>())
+        {
+            // We already handle single items, so check for ItemCount > 1
+            if (!firstItemFound)
+            {
+                firstItemFound = true;
+                item.Classes.Set("First", item.IsVisible && ItemCount > 1);
+            }
+            item.Classes.Set("Last", false);
+            item.Classes.Set("SingleItem", false);
+        }
+
+        var lastVisibleItem = Items.OfType<Control>().LastOrDefault(x => x.IsVisible);
+        lastVisibleItem?.Classes.Set("Last", ItemCount > 1);
     }
 }
