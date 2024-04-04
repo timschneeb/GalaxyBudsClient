849c8e109a97b8c470a95f38471af6fe65522d74
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-04-04 03:42:26 +0200

    refactor: optimize SppMessageHandlerFactory using type cache

--- GalaxyBudsClient/Message/SppMessageHandlerFactory.cs
+++ GalaxyBudsClient/Message/SppMessageHandlerFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using GalaxyBudsClient.Message.Decoder;
 using GalaxyBudsClient.Message.Encoder;
@@ -26,39 +27,63 @@ public static class SppMessageHandlerFactory
     private static readonly Type[] RegisteredDecoders;
     private static readonly Type[] RegisteredEncoders;
 
+    private static readonly Dictionary<MsgIds, Type?> DecoderTypeCache = new();
+    
     public static BaseMessageDecoder? CreateDecoder(SppMessage msg, Models model)
     {
         BaseMessageDecoder? b = null;
-        foreach (var t in RegisteredDecoders)
+
+        if (DecoderTypeCache.TryGetValue(msg.Id, out var cachedType))
         {
-            var act = Activator.CreateInstance(t);
-            if (act?.GetType() != t) 
-                continue;
+            // If cached type is null, no decoder exists for message id
+            if (cachedType == null)
+                return null;
                 
-            var decoder = (BaseMessageDecoder)act;
-            if (decoder.HandledType != msg.Id) 
-                continue;
+            b = (BaseMessageDecoder?)Activator.CreateInstance(cachedType);
+        }
 
-            decoder.TargetModel = model;
-                
-            SentrySdk.ConfigureScope(scope =>
+        if (b == null)
+        {
+            foreach (var t in RegisteredDecoders)
             {
-                scope.SetTag("msg-data-available", "true");
-                scope.SetExtra("msg-type", msg.Type.ToString());
-                scope.SetExtra("msg-id", msg.Id);
-                scope.SetExtra("msg-size", msg.Size);
-                scope.SetExtra("msg-total-size", msg.TotalPacketSize);
-                scope.SetExtra("msg-crc16", msg.Crc16);
-                scope.SetExtra("msg-payload", HexUtils.Dump(msg.Payload, 512, false, false, false));
-            });
+                var act = Activator.CreateInstance(t);
+                if (act?.GetType() != t)
+                    continue;
 
-            decoder.Decode(msg);
-            b = decoder;
-            break;
+                var decoder = (BaseMessageDecoder)act;
+                if (decoder.HandledType != msg.Id)
+                    continue;
+
+                b = decoder;
+                // Cache decoder type for message id
+                DecoderTypeCache.TryAdd(msg.Id, t);
+                break;
+            }
+
+            if (b == null)
+            {
+                // No decoder for message id exists
+                DecoderTypeCache.TryAdd(msg.Id, null);
+            }
         }
 
         if (b == null) 
             return b;
+        
+        b.TargetModel = model;
+                
+        SentrySdk.ConfigureScope(scope =>
+        {
+            scope.SetTag("msg-data-available", "true");
+            scope.SetExtra("msg-type", msg.Type.ToString());
+            scope.SetExtra("msg-id", msg.Id);
+            scope.SetExtra("msg-size", msg.Size);
+            scope.SetExtra("msg-total-size", msg.TotalPacketSize);
+            scope.SetExtra("msg-crc16", msg.Crc16);
+            scope.SetExtra("msg-payload", HexUtils.Dump(msg.Payload, 512, false, false, false));
+        });
+
+        b.Decode(msg);
             
         foreach (var hook in ScriptManager.Instance.DecoderHooks)
         {
