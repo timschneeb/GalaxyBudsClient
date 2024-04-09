b5e8453156961a9556ae04d7b7a0e75efd36807f
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-04-09 15:14:51 +0200

    fix: null handling

--- GalaxyBudsClient.Generators/Enums/RootSourceGenerationHelper.cs
+++ GalaxyBudsClient.Generators/Enums/RootSourceGenerationHelper.cs
@@ -66,7 +66,7 @@ public static class RootSourceGenerationHelper
                          _ => "<unknown>"
                          """);
         
-        gen.LeaveScope(";");
+        gen.LeaveScope(" ?? \"<unknown>\";");
         gen.LeaveScope();
 
         // GetValuesByType
