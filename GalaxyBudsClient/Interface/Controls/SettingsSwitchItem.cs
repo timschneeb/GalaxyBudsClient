bcb18c1037287bb2d640cddba016783649389897
YexuanXiao <bizwen@nykz.org>
2024-06-17 11:38:35 +0800

    Adding i18n support to SettingsSwitchItem

--- GalaxyBudsClient/Interface/Controls/SettingsSwitchItem.cs
+++ GalaxyBudsClient/Interface/Controls/SettingsSwitchItem.cs
@@ -3,6 +3,8 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using GalaxyBudsClient.Generated.I18N;
+using GalaxyBudsClient.Utils.Interface;
 
 namespace GalaxyBudsClient.Interface.Controls;
 
@@ -13,24 +15,33 @@ public class SettingsSwitchItem : SettingsSymbolItem
     {
         _toggle = new ToggleSwitch();
         _toggle.IsCheckedChanged += (_, _) => IsChecked = _toggle.IsChecked;
-        
+
         Click += OnClick;
         IsClickEnabled = true;
         Footer = _toggle;
+
+        Loc.LanguageUpdated += OnLanguageUpdated;
+        OnLanguageUpdated();
     }
 
     private readonly ToggleSwitch _toggle;
-    
-    public static readonly RoutedEvent<RoutedEventArgs> IsCheckedChangedEvent = 
+
+    public static readonly RoutedEvent<RoutedEventArgs> IsCheckedChangedEvent =
         RoutedEvent.Register<SettingsSwitchItem, RoutedEventArgs>(nameof(IsCheckedChanged), RoutingStrategies.Bubble);
 
-    public static readonly StyledProperty<bool?> IsCheckedProperty = 
+    public static readonly StyledProperty<bool?> IsCheckedProperty =
         ToggleButton.IsCheckedProperty.AddOwner<SettingsSwitchItem>();
-    
+
     public event EventHandler<RoutedEventArgs>? IsCheckedChanged
     {
         add => AddHandler(IsCheckedChangedEvent, value);
         remove => RemoveHandler(IsCheckedChangedEvent, value);
+    }
+
+    private void OnLanguageUpdated()
+    {
+        _toggle.OnContent = Strings.On;
+        _toggle.OffContent = Strings.Off;
     }
 
     public bool? IsChecked
@@ -38,13 +49,13 @@ public class SettingsSwitchItem : SettingsSymbolItem
         get => GetValue(IsCheckedProperty);
         set => SetValue(IsCheckedProperty, value);
     }
-    
+
     private void OnClick(object? sender, RoutedEventArgs e)
     {
         IsChecked = !IsChecked;
         RaiseEvent(new RoutedEventArgs(IsCheckedChangedEvent));
     }
-    
+
     protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
     {
         if (change.Property == IsCheckedProperty)
