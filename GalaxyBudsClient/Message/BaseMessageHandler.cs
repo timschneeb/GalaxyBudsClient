6242fbacd0f876f331212ffe9fdd4c7b262d22fb
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-07-05 15:27:00 +0200

    feat: add new buds3 related msg ids

--- GalaxyBudsClient/Message/BaseMessageHandler.cs
+++ GalaxyBudsClient/Message/BaseMessageHandler.cs
@@ -1,3 +1,19 @@
 ﻿namespace GalaxyBudsClient.Message;
 
 public abstract class BaseMessageHandler : MessageAsDictionary;
+
+/* TODO implement new decoders
+ 
+MsgAdaptiveEqStatus
+MsgCustiomEqualizeSend
+MsgEqualizeCustomRecv
+MsgExtraHighAmbient
+MsgSDTestResult
+MsgSelfDiagnostics
+MsgSetHearingTestResult
+MsgSetHotCommand
+MsgOverheat
+MsgVoiceWakeUpListeningStatus
+HearingTestManager
+ 
+*/
