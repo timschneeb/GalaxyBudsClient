a58148049c9a4092e0543712544dc4709a750586
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-07-25 21:45:57 +0200

    chore: update build script

--- GalaxyBudsClient.Android/build.sh
+++ GalaxyBudsClient.Android/build.sh
@@ -5,4 +5,8 @@ else
   exit 1
 fi
 
-dotnet publish -f net8.0-android -c Debug -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=/home/tim/Development/_KeyStores/keystore.jks -p:AndroidPackageFormats=$1 -p:AndroidSigningKeyAlias=galaxybudsclient -p:AndroidSdkDirectory=/home/tim/Android/Sdk -p:AndroidSigningKeyPass="file:/home/tim/.keys/jks" -p:AndroidSigningStorePass="file:/home/tim/.keys/jks" -p:DebugType=PdbOnly -p:NotDebuggable=true -p:EmbedAssembliesIntoApk=true -p:$2
+if [[ -z $PASS ]]; then
+  MY_SCRIPT_VARIABLE="Env var PASS to decrypt keystore not set"
+fi
+
+dotnet publish -f net8.0-android -c Debug -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=/home/tim/Development/_KeyStores/keystore.jks -p:AndroidPackageFormats=$1 -p:AndroidSigningKeyAlias=galaxybudsclient -p:AndroidSdkDirectory=/home/tim/Android/Sdk -p:AndroidSigningKeyPass="env:PASS" -p:AndroidSigningStorePass="env:PASS" -p:DebugType=PdbOnly -p:NotDebuggable=true -p:EmbedAssembliesIntoApk=true -p:$2 -v d
